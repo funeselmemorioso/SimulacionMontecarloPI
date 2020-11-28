1419a704a7eac5b5d6322dee32d44245a54f7873
ezequiel veltri <ezequiel.veltri@istea.com.ar>
2020-11-28 13:41:41 -0300

    git remote add origin https://github.com/funeselmemorioso/MaquinaDeDalton.gii

---
+++ MontecarloPI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MontecarloPI
+{
+    class Program
+    {
+        static void Main()
+        {
+            // http://paginas.fisica.uson.mx/laura.yeomans/dex2/pi.pdf
+            // https://www.i-ciencias.com/pregunta/31476/como-saber-si-un-punto-esta-dentro-de-un-circulo
+
+            Random aleatorio = new Random();
+            int cantPuntoCuadrado = 100000000;
+            int cantPuntoCirculo = 0;           
+            double x = 0.0;
+            double y = 0.0;
+
+            for (int i = 0; i < cantPuntoCuadrado; i++)
+            {
+                x = aleatorio.NextDouble();
+                y = aleatorio.NextDouble();
+
+                if ((Math.Sqrt(x * x + y * y) <= 1.0))                
+                    cantPuntoCirculo++;   
+            }
+            Console.WriteLine("Aproximación de PI es: " + (Double)(4 * cantPuntoCirculo) / cantPuntoCuadrado);      
+        }       
+    }
+}
